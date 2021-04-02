dc8adc7be5cd183bbaf700718521cdf20977f0d8
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-04-02 12:07:01 +0900

    CheckStupidNum

--- NABEATSU-of-the-world/src/main/CheckStupidNum.java
+++ NABEATSU-of-the-world/src/main/CheckStupidNum.java
@@ -2,7 +2,18 @@ package main;
 
 public class CheckStupidNum {
 
-	public static void checkStupidNum(){
+	public static void checkStupidNum(int num){
 
+		//System.out.println(num);
+
+		for (int i = 1; i <= num; i++) {
+
+			// 3の倍数または、3のつく数字の時アホになる
+			if (i % 3 == 0 || Integer.toString(i).contains("3")) {
+				System.out.println(i + "っっ！！");
+			} else {
+				System.out.println(i);
+			}
+		}
 	}
 }
--- NABEATSU-of-the-world/src/main/Main.java
+++ NABEATSU-of-the-world/src/main/Main.java
@@ -2,8 +2,34 @@ package main;
 
 public class Main {
 
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
 
+		// 引数チェック
+		if (args.length == 0) {
+			throw new ArrayIndexOutOfBoundsException("引数として数字を入力してください");
+		}
+		// 引数を数値に変換
+		int num = parseToint(args[0]);
+
+		// 数字を判定する
+		CheckStupidNum csn = new CheckStupidNum();
+		csn.checkStupidNum(num);
 	}
 
+	/**
+	 * 引数を数値に変換
+	 * @param arg コマンドライン引数
+	 * @return 数値に変換された値
+	 */
+	public static int parseToint(String arg) {
+
+		int num = 0;
+		try {
+			num = Integer.parseInt(arg);
+		} catch (NumberFormatException ex) {
+			throw new NumberFormatException("数値に変換できません");
+		}
+		return num;
+	}
 }
