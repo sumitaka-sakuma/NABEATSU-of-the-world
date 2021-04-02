551503a56d01c710e935ed32930a1fc0c27e5d1a
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-04-02 15:58:54 +0900

    commented

--- NABEATSU-of-the-world/src/main/CheckStupidNum.java
+++ NABEATSU-of-the-world/src/main/CheckStupidNum.java
@@ -12,20 +12,22 @@ public class CheckStupidNum {
 	public static void checkStupidNum(int num){
 
 		NumSetterGetter nsg = new NumSetterGetter();
+
+		// 普通の数字のリスト
 		List<Integer> normalNumList = new ArrayList<>();
+		// アホな数字のリスト
 		List<Integer> stupidNumList = new ArrayList<>();
 
 		for (int i = 1; i <= num; i++) {
-
 			// 3の倍数または、3のつく数字の時アホになる
 			if (i % 3 == 0 || Integer.toString(i).contains("3")) {
 				stupidNumList.add(i);
 				nsg.setStupidNumList(stupidNumList);
-				//System.out.println(i + "っっ！！");
+				System.out.println(i + "っっ！！");
 			} else {
 				normalNumList.add(i);
 				nsg.setNormalNumList(normalNumList);
-				//System.out.println(i);
+				System.out.println(i);
 			}
 		}
 	}
